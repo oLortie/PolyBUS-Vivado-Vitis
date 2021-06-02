d0fbf036633f0ad402ac71ac0b37396b4a7dec42
Olivier Lortie <olivier.lortie2@usherbrooke.ca>
2021-06-02 11:43:46 -0400

    Corrected bug that did not convert raw data into voltage

--- vitisProj/s4i_tools.h
+++ vitisProj/s4i_tools.h
@@ -8,12 +8,16 @@
 #ifndef SRC_S4I_TOOLS_H_
 #define SRC_S4I_TOOLS_H_
 
+#include <xgpio.h>
+
 #define S4I_NUM_SWITCHES	4
 
 void			s4i_init_hw();
 int 			s4i_is_cmd_sws(char *buf);
 int             s4i_is_cmd_respiration(char *buf);
 unsigned int 	s4i_get_sws_state();
+u16 s4i_getSampleRespirationRaw();
+float s4i_GetRespirationVoltage();
 
 
 #endif /* SRC_S4I_TOOLS_H_ */

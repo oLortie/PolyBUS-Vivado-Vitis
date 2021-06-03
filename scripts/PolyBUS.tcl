21d15a8f405f1c632a65d256291d13c07ac32ada
Olivier Lortie <olivier.lortie2@usherbrooke.ca>
2021-06-03 19:43:11 -0400

    Corrected Tcl for project creation

--- scripts/PolyBUS.tcl
+++ scripts/PolyBUS.tcl
@@ -188,7 +188,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/..ipRepo/myADCip_1.0"] [file normalize "$origin_dir/../vivado-library-2020.1"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ipRepo/myADCip_1.0"] [file normalize "$origin_dir/../vivado-library-2020.1"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild

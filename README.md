f8edddaefb26804f5f25b29c258e395dd3552ed3
Francois Marcoux <marf2910@usherbrooke.ca>
2021-06-03 21:40:18 -0400

    Add script for generating VHDL samples
    Modification du read me pour plus d'information sur le script et sur la génération du projet vivado a partir des tcl

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 PolyBUS
+
+
+
+****INFORMATION POUR GENERER LE PROJET VIVADO A PARTIR DES TCL*****
+1. ouvrir vivado
+2. Aller dans la console TCL et s'assurer d'être dans le bon folder ( .../PolyBUS/scripts) 
+3. executer la commande suivante : source ./PolyBUS.tcl
+
+
+**INFORMATION POUR SETUP DU SCRIPT DE GENERATION DE SIGNAUX**
+1. Ouvrir une console de commande
+2. verifier que python est bien installé sur votre ordinateur avec la commande : python 
+	Si python est bien installé, il devrait vous afficher la version 
+3. installer pip si ce n'est pas deja fait
+4. Installer le package neurokit2 : pip install neurokit2
+	Normallement, tous les package nécéssaire sont installé avec neurokit2
+5. Aller dans le répertoire PolyBUS/scripts
+6. Executer la commande suivante pour vérifier que tous les packages nécéssaires sont installés :
+	python SignalGenerator.py -h
+7. Vous devriez voir un message d'aide qui vous montres quels paramètres peuvent être modifié
+
+8. Vous être prêt a générer des signaux, voir la section suivante pour plus d'information sur les paramètres et les valeurs par défaut
+
+9. Lors de l'execution du script, un pop-up montre l'allure du signal demandé
+
+10. Les echantillons VHDL sont inscrit de un fichier .txt dans le même répertoire que le script
+Le nom du fichier va varier selon le signal choisi
+Signalpo.txt pour un signal de pouls
+
+**INFORMATION SUR LES PARAMÈTRES DU SCRIPT DE GÉNÉRATION DES SIGNAUX**** 
+1. Le seul paramètre obligatoire a fournir est le type de signaux que l'on veut simuler
+	1.1 Pour simuler un signal de Pouls , utiliser "po"
+	1.2 Pour un signal de Pression artérielle : utiliser "pr"
+	1.3 Pour un signal de respiration : utiliser "re"
+	1.4 Pour un signal de perspiration : utiliser "re"
+	Ce paramètre est inscrit directement après le .py dans la ligne de commande
+	exemple : python SignalGenerator.py "po"  
+	Cette ligne va génerer un signal représentant le pouls avec les valeurs par défaut (signal d'une durée de 1 seconde a 70 Bpm)
+
+2. Le paramètre -t représente la durée du signal simulé en seconde et indirectement le nombre d'échantillons total inscrit de le fichier .txt ** type int**
+	valeur par défault : 1 seconde
+3. Le paramèetre -f représente la fréquence d'échantillonnage de nos modules ADC/DAC en Hz **type int**
+	valeur par défault : 100Hz
+4. Le paramètre -b représente le nombre de battement par minute ** type int**
+	valeur par défault : 70 bpm
+	Ce paramètre est utilisé lors de la généraion d'un signal de pouls et d'un signal de pression artérielle
+5. Le paramètre -r représente la fréquence (Hz) du sinus de la respiration ** type float** 
+	valeur par défaut : 0.25 
+6. Les paramètres -S et -D sont la systolique et Diastolique  respecitvement **  type int **
+	***Le maximum convenu est, de 140 pour la systolique  et de 90 pour la diastolique.**
+	***Si vous entrez des valeurs supérieurs, ca va faire chier les echantillons VHDL car certains vont dépasser 3.3 V et ne seront plus sur 12 bit***
+	valeur par défault : 120 et 80
+7. Les paramètres -p et -s sont liés au signal de perspiration ** type float et int respecitvement**
+	-p indique le taux de variation de la fonction 
+	-s indique le nombre d'échantillons (step) que le signal va prendre pour varier d'un point a l'autre
+	exemple -p =1  -s =3
+	Le signal de pespiration va utiliser 3 echantillons pour passer de x a x +/- p/2 (valeur aléatoire entre +p/2 et -p/2) 
+	Le signal restera toujours entre 3.3 et 0
+	valeur par défaut : 1 et 1 
+
+8. exemple de paramètre 
+
+Pouls :  python SignalGenerator.py "po"  -t 5 -b 90 
+	Génère un signal de  90 bpm pendant 5 seconde
+Pression : python SignalGenerator.py "pr" -t 4 -S 110 -D 70
+	Génère un signal avec 110 en systolique et 70 en diastolique a une fréquence de 70 bpm (valeur par défault)
+Respiration : python SignalGenerator.py "re" -t 6 -r 0.75 
+	Génère un sinus a 0.75 Hz (1.3333333 respiration par seconde) pendant 
+Pespiration : python SignalGenerator.py "pe" -p 0.5 -s 5
+	Génère un signal qui débute aléatoirement entre 3.3 et 0. 
+	Le signal va varier de +/- 0.5 V à tous les 5 échantillons
---
+++ scripts/SignalGenerator.py
@@ -0,0 +1,125 @@
+import warnings
+import random
+import argparse as ag
+import neurokit2 as nk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+
+
+
+
+
+def genPespiration(variation,step,frequence,temps):
+    array = []
+    current_pesp = random.uniform(VOLTAGE_MIN, VOLTAGE_MAX)
+    for i in range(0,frequence*temps,step):
+        delta = random.uniform( -(variation/2.0),variation/2.0)
+        for y in range(0,step) :
+            current_pesp = current_pesp+ float(delta/step)
+            if current_pesp > VOLTAGE_MAX:
+                current_pesp = VOLTAGE_MAX
+            elif current_pesp < VOLTAGE_MIN:
+                current_pesp = VOLTAGE_MIN
+            array.append(current_pesp)
+    return array
+
+def generateVHDLsamples (array,name):
+    y_int = [int(y_i/3.3001 * pow(2,12)) for y_i in array]
+    y_hex = ["{0:03X}".format(y_i,2) for y_i in y_int]
+    f_vhdl =open("Signal{name}.txt".format(name=name),"w");
+
+    for i in range(len(y_hex)):
+        data = "x\"{}\"".format(y_hex[i])
+        f_vhdl.write(data)
+        f_vhdl.write(",\n")
+    f_vhdl.close()
+
+def keepOnlyPositive(array):
+    result = []
+    for i in array :
+        if i < 0 :
+            result.append(0)
+        else :
+            result.append(i)
+    return result
+
+
+VOLTAGE_MAX = 3.3
+VOLTAGE_MIN = 0
+MAX_SYSTOLIQUE = 140
+MAX_DIASTOLIQUE = 90
+warnings.filterwarnings('ignore')
+
+plt.rcParams['figure.figsize'] = [10, 6]  # Bigger images
+
+parser= ag.ArgumentParser("Script de génération des échantillons VHDL qui simulent différent type de signaux corporels \n "
+                          "Le script peut générer : Pression artérielle, Pouls, respiration,perspiration \n"
+                          "Les valeurs par défault pour les signaux à générer sont : \n "
+                          "Pouls :  70 bpm , Pression : 120/80, respiration : 0.25Hz (1 respiraiton au 4 seconde , pespiration : 1 (taux de variation entre chaque echantillon)")
+
+parser.add_argument("T",type = str,help = "Le type du signal a généré : les choix sont : pr ,po, re,pe \n pr = Pression, \n po = Pouls, \n re = respiration, \n pe= pespiration")
+parser.add_argument( "-t", type = int , default = 1, help = "La durée en seconde du signal généré")
+parser.add_argument("-f" ,default = 100 ,type =int, help = "La fréquence d'échantillonage des modules DAC/ADC")
+parser.add_argument( "-b" ,type =int, default = 70, help = "le nombre de battement par minutes\n")
+parser.add_argument("-r",  type=float, default = 0.25,help="la fréquence en hz de la respiration \n")
+parser.add_argument("-D",  type=int, default = 80 ,help="La distolique du patient (deuxieme peak dans le signal de la pression artérielle)  \n")
+parser.add_argument("-S",  type=int, default = 120,help="la systolique du patient (premier peak dans le signal de la pression artérielle)  \n")
+parser.add_argument("-p",  type=float, default = 1,help="le taux de variation  +/- de la pespiration\n")
+parser.add_argument("-s",  type=int, default = 1,help="le nombre d'echantillon identique avant une variation dans le signal de la pespiration\n")
+args = parser.parse_args()
+
+signaltype =vars(args)["T"]
+t = vars(args)["t"]
+f = vars(args)["f"]
+bpm = vars(args)["b"]
+diastolic = vars(args)["D"]
+systolic = vars(args)["S"]
+respiration = vars(args)["r"]
+pespiration = vars(args)["p"]
+step = vars(args)["s"]
+print(signaltype,t,f,bpm,diastolic,systolic,respiration,pespiration)
+ech_array = []
+
+if "po" in signaltype:
+    #procedure pour generer un pouls
+    samples_pouls =  nk.ecg_simulate(duration=t, sampling_rate=f ,noise=.05, heart_rate=bpm,method="simple")
+    ech_array = keepOnlyPositive(samples_pouls)
+    ech_array /= np.max(ech_array,axis=0)/3.3
+
+elif "pr" in signaltype :
+
+    index = np.linspace(-9, 9, num= int(f / (bpm/60)))
+    normal1 = scipy.stats.norm.pdf(index, loc=-2, scale=1)*  (systolic/MAX_SYSTOLIQUE)#premier peak systolique
+    normal2 = scipy.stats.norm.pdf(index, loc=2, scale=1.5)* (diastolic/MAX_DIASTOLIQUE) # deuxieme peak diastolique
+    samples = 8 * (normal1 + normal2) #facteur 8 seulement pour scaler entre 0 et 3.3
+
+    # La valeur en Voltage d'une personne qui a une systolique normal (120), est autour de  2.8 , pour une systolique eleve(140), on est autour de 3.2 , une sytolique basse (100) est autour de 2.3
+    #la valeur en Voltage d'une personne qui a une diastolique normal (80) est autour de 1.8 ,  pour une diastolique eleve(90), on est atour de 2.10, une diastolique basse (60) est autour de  1.4
+
+    #prolongement du signal de pression selon le temps de signal voulu
+    for i in range(0,t):
+        for y in samples:
+            ech_array.append(y)
+
+elif "re" in signaltype:
+    t_sin = np.arange(t*f)
+    ech_array = 1.65 * np.sin(respiration * (2 * np.pi * t_sin) / f) + 1.65 # le plus 1 est pour ne pas avoir de valeur negative
+elif "pe" in signaltype:
+    ech_array = genPespiration(pespiration,step,f,t)
+else :
+    ValueError("The fuck is that signal type ??  give me a valid one , you gave me  {}".format(signaltype))
+
+# # Visualize
+ecg = pd.DataFrame({"{}".format(signaltype): ech_array})
+
+nk.signal_plot(ecg)
+#
+plt.show()
+
+generateVHDLsamples(ech_array,signaltype)
+
+
+
+

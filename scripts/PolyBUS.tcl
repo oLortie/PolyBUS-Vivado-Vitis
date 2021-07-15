5a8578b8161711f7b22101020436be3205572c8b
Olivier Lortie <olivier.lortie2@usherbrooke.ca>
2021-07-15 11:10:17 -0400

    Modified TCL for new IP

--- scripts/PolyBUS.tcl
+++ scripts/PolyBUS.tcl
@@ -68,7 +68,7 @@ proc checkRequiredFiles { origin_dir} {
   }
 
   set paths [list \
-   [file normalize "$origin_dir/../ipRepo/myADCip_1.0"] \
+   [file normalize "$origin_dir/../ipRepo/PolyBUSip_1.0] \
    [file normalize "$origin_dir/../vivado-library-2020.1"] \
   ]
   foreach ipath $paths {
@@ -192,7 +192,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ipRepo/myADCip_1.0"] [file normalize "$origin_dir/../vivado-library-2020.1"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ipRepo/PolyBUSip_1.0"] [file normalize "$origin_dir/../vivado-library-2020.1"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
